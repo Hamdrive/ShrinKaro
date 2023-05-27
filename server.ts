1177258b4e3a9f508e7ec179b4b9d193068d9329
Hamdrive <hamza.husein017@gmail.com>
2023-05-27 18:24:55 +0530

    [update] setup cors

--- server.ts
+++ server.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import router from './router'
+import cors from 'cors'
 
 const app = express()
 
 const PORT = process.env.PORT || 5000
 
+app.use(cors())
 app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => res.status(200).send('Hello World, Server is live'))
