8c06e50e47e49edb60fa5933dc9ff7c202fcfa2f
Hamdrive <hamza.husein017@gmail.com>
2023-05-27 18:25:54 +0530

    [feat] shrink route w/db create

--- router.ts
+++ router.ts
@@ -1,15 +1,40 @@
 import { Router } from "express";
+import { validationResult, body } from "express-validator";
+import { init } from "@paralleldrive/cuid2";
+import prisma from "./db";
 
 const router = Router();
 
 // Shorten
-router.post("/url", (req, res) => {
-  //http://localhost:5000/api/url?q=abc.com
-  const { q } = req.query;
-  res.json({
-    message: `Received your url ${q}, and stored in DB, here's your code`,
-  });
-});
+router.post(
+  "/shrink",
+  body("url").isURL({
+      require_tld: true,
+      require_valid_protocol: true,
+      protocols: ["https"],
+  }),
+  async (req, res) => {
+      //http://localhost:5000/api/shrink, {body: url: "https://www.abc.com"}
+      const { url } = req.body;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.status(400);
+        res.json({ errors: errors.array() });
+    }
+    
+    const createId = init({ length: 6 });
+    const generateShrinKode = createId();
+
+    const shrinkUrl = await prisma.link.create({
+      data: { fullUrl: url, shrinKode: generateShrinKode },
+    });
+
+    res.status(201).json({
+      data: shrinkUrl,
+    });
+  }
+);
 
 // Get + Redirect
 router.get("/id", (req, res) => {
