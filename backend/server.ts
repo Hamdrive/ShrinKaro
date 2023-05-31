228264007fb0fd9ace64ef57a2020bd908121fff
Hamdrive <hamza.husein017@gmail.com>
2023-05-31 14:19:24 +0530

    [refactor] undo cors setting

--- backend/server.ts
+++ backend/server.ts
@@ -6,7 +6,7 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.use(cors({ origin: "http://localhost:3000/" }));
+app.use(cors());
 app.use(express.urlencoded({extended: true}))
 app.use('/api', router)
 
