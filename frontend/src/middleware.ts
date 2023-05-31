b4bb4c1cf7a592c094c108aca0cdd097bd6a8c9c
Hamdrive <hamza.husein017@gmail.com>
2023-05-31 14:31:12 +0530

    [feat] middleware to handle redirects

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const regex = new RegExp("^/[a-zA-Z0-9]{6}$");
+  const pathname = req.nextUrl.pathname;
+
+  if (regex.test(req.nextUrl.pathname)) {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_REDIRECT}/${pathname}`,
+        { method: "GET" }
+      );
+      if (res.status === 200) {
+        const data = await res.json();
+        return NextResponse.redirect(data.actualURl);
+      } else {
+        return NextResponse.redirect(req.nextUrl.origin);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+export const config = {
+  matcher: ["/((?!.*\\.).*)"],
+};
---
+++ frontend/src/pages/[shrinKode]/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div>If you have landed here, something went very wrong.Go back to homepage.</div>
+  )
+}
+
+export default Index
